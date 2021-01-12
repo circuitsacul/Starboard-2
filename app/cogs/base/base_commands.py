23369c1b1512ed5c42870d356d0eb12f71380b13
Lucas <circuitsacul@gmail.com>
2021-01-12 10:25:54 -0500

    add ping command

--- app/cogs/base/base_commands.py
+++ app/cogs/base/base_commands.py
@@ -4,19 +4,37 @@ from discord.ext import commands
 from ...classes.bot import Bot
 
 
+def ms(seconds: int) -> int:
+    return round(seconds*1000, 1)
+
+
 class Base(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
     @commands.command(
-        name='test'
+        name='ping', aliases=['latency'],
+        brief="Shows current clusters and shards latency"
     )
-    async def test(
-        self,
-        ctx: commands.Context,
-        starboard: discord.TextChannel
-    ) -> None:
-        pass
+    async def ping(self, ctx: commands.Context) -> None:
+        cluster = self.bot.cluster_name
+        shard = self.bot.get_shard(ctx.guild.shard_id)
+
+        embed = discord.Embed(
+            title='Pong!',
+            color=self.bot.theme_color
+        )
+        embed.add_field(
+            name=f"Cluster **{cluster}**",
+            value=f"{ms(self.bot.latency)} ms",
+            inline=False
+        )
+        embed.add_field(
+            name=f"Shard **{shard.id}**",
+            value=f"{ms(shard.latency)} ms",
+            inline=False
+        )
+        await ctx.send(embed=embed)
 
 
 def setup(bot: Bot) -> None:
