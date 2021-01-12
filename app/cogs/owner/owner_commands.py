dc24ed8ddea6f6d8e81ae77b44b344242485ad7e
Lucas <circuitsacul@gmail.com>
2021-01-12 13:50:05 -0500

    remove shutdown command

--- app/cogs/owner/owner_commands.py
+++ app/cogs/owner/owner_commands.py
@@ -8,20 +8,6 @@ class OwnerCommands(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
-    @commands.command(name='shutdown')
-    @commands.is_owner()
-    async def shutdown_bot(
-        self,
-        ctx: commands.Context
-    ) -> None:
-        await ctx.send("Are you sure?")
-        if await utils.confirm(ctx):
-            await ctx.send("Shutting down...")
-            cmd: commands.Command = self.bot.get_command('evall')
-            await ctx.invoke(cmd, body='exit(0)')
-            return
-        await ctx.send("Cancelled")
-
     @commands.command(name='restart')
     @commands.is_owner()
     async def restart_bot(
