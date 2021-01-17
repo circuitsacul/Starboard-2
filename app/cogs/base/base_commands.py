24ed7987e31fd17b7f0b64df01dd0325d74c7c2f
Lucas <circuitsacul@gmail.com>
2021-01-16 21:50:10 -0500

    add vote links

--- app/cogs/base/base_commands.py
+++ app/cogs/base/base_commands.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 
 from app.utils import ms
+import config
 
 from ...classes.bot import Bot
 
@@ -35,6 +36,29 @@ class Base(commands.Cog):
             name=f"Shard **{shard.id}**",
             value=f"{ms(shard.latency)} ms",
             inline=False
+        )
+        await ctx.send(embed=embed)
+
+    @commands.command(
+        name='links', aliases=['invite', 'support'],
+        brief="Lists important/useful links"
+    )
+    async def links(self, ctx: commands.Context) -> None:
+        embed = discord.Embed(
+            title="Important Links",
+            color=self.bot.theme_color
+        ).add_field(
+            name="Invite Links",
+            value=(
+                f"**[Support Server]({config.SUPPORT_INVITE})**\n"
+                f"**[Invite Starboard]({config.BOT_INVITE})**\n"
+            )
+        ).add_field(
+            name="Vote Links",
+            value='\n'.join(config.VOTE_LINKS)
+        ).add_field(
+            name="Review Links",
+            value='\n'.join(config.REVIEW_LINKS)
         )
         await ctx.send(embed=embed)
 
