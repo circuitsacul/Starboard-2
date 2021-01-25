a2588b00beffc9ed29846b15dae877471f92101f
Lucas <circuitsacul@gmail.com>
2021-01-25 12:19:04 -0500

    add cooldown to aschannels

--- app/cogs/autostarchannels/asc_events.py
+++ app/cogs/autostarchannels/asc_events.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord.ext import commands
 
@@ -9,15 +11,27 @@ from . import asc_funcs
 class AutoStarEvents(commands.Cog):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        self.cooldown = commands.CooldownMapping.from_cooldown(
+            3, 10, commands.BucketType.channel
+        )
 
     @commands.Cog.listener()
-    @commands.cooldown(5, 10, type=commands.BucketType.channel)
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot:
             return
         aschannel = await self.bot.db.get_aschannel(message.channel.id)
         if not aschannel:
             return
+
+        bucket = self.cooldown.get_bucket(message)
+        retry_after = bucket.update_rate_limit(
+            message.created_at.replace(
+                tzinfo=datetime.timezone.utc
+            ).timestamp()
+        )
+        if retry_after:
+            return
+
         await asc_funcs.handle_message(self.bot, message, aschannel)
 
 
