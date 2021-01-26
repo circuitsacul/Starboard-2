9f81e10399edae08e488768fe161eb199f95da76
Lucas <circuitsacul@gmail.com>
2021-01-26 13:06:33 -0500

    readability

--- app/cogs/quick_actions/qa_funcs.py
+++ app/cogs/quick_actions/qa_funcs.py
@@ -3,16 +3,16 @@ from typing import Optional
 
 def get_qa_type(emoji: str, guild: dict) -> Optional[str]:
     qa_type: Optional[str] = None
-    if emoji == guild["qa_force"]:
-        qa_type = "qa_force"
-    elif emoji == guild["qa_unforce"]:
-        qa_type = "qa_unforce"
-    elif emoji == guild["qa_freeze"]:
-        qa_type = "qa_freeze"
-    elif emoji == guild["qa_trash"]:
-        qa_type = "qa_trash"
-    elif emoji == guild["qa_recount"]:
-        qa_type = "qa_recount"
-    elif emoji == guild["qa_save"]:
-        qa_type = "qa_save"
+    to_check = [
+        "qa_force",
+        "qa_unforce",
+        "qa_freeze",
+        "qa_trash",
+        "qa_recount",
+        "qa_save",
+    ]
+    for qat in to_check:
+        if emoji == guild[qat]:
+            qa_type = qat
+            break
     return qa_type
