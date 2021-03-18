aa1aaf7d791a1d0b4face477aa4ddcbb5f9f2d6e
Circuit <circuitsacul@gmail.com>
2021-03-17 20:41:34 -0400

    fix bugs with leaderboard

--- app/cogs/fun/fun_funcs.py
+++ app/cogs/fun/fun_funcs.py
@@ -8,24 +8,28 @@ async def get_guild_leaderboard(bot: Bot, guild: discord.Guild) -> dict:
     top_users = await bot.db.fetch(
         """SELECT * FROM members
         WHERE guild_id=$1
+        AND xp > 0
         ORDER BY xp DESC
         LIMIT 200""",
         guild.id,
     )
     uids = [int(u["user_id"]) for u in top_users]
     user_lookup = await bot.cache.get_members(uids, guild)
-    for x, u in enumerate(top_users):
+    current_rank = 0
+    for u in top_users:
         obj = user_lookup.get(int(u["user_id"]))
         if not obj:
             continue
         if obj.bot:
             continue
 
+        current_rank += 1
+
         leaderboard[obj.id] = {
             "name": str(obj),
             "xp": u["xp"],
             "level": u["level"],
-            "rank": x + 1,
+            "rank": current_rank,
         }
     return leaderboard
 
