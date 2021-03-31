ac8173aad25467e3c60bbcbdbe812e033bf8c73a
Circuit <circuitsacul@gmail.com>
2021-03-31 16:00:31 -0400

    put global checks in globa_checks cog

--- app/checks.py
+++ app/checks.py
@@ -1,7 +1,6 @@
 from discord.ext import commands
 
 import config
-from app import errors
 
 
 def is_owner() -> callable:
@@ -11,27 +10,3 @@ def is_owner() -> callable:
         return True
 
     return commands.check(predicate)
-
-
-# Global Checks
-async def not_disabled(ctx: commands.Context) -> bool:
-    if ctx.guild is None:
-        return True
-    if ctx.channel.permissions_for(ctx.message.author).manage_guild:
-        return True
-    guild = await ctx.bot.db.guilds.get(ctx.guild.id)
-    if not guild["allow_commands"]:
-        raise errors.AllCommandsDisabled()
-    name = ctx.command.qualified_name
-    if name in guild["disabled_commands"]:
-        raise errors.CommandDisabled(
-            f"The command {name} has been disabled "
-            "by the moderators of this server."
-        )
-    return True
-
-
-GLOBAL_CHECKS = [
-    not_disabled,
-    commands.bot_has_permissions(send_messages=True),
-]
--- app/cogs/global_checks.py
+++ app/cogs/global_checks.py
@@ -1,8 +1,33 @@
+from discord.ext import commands
+
 from app.classes.bot import Bot
 
-from app import checks
+from app import errors
+
+
+async def not_disabled(ctx: commands.Context) -> bool:
+    if ctx.guild is None:
+        return True
+    if ctx.channel.permissions_for(ctx.message.author).manage_guild:
+        return True
+    guild = await ctx.bot.db.guilds.get(ctx.guild.id)
+    if not guild["allow_commands"]:
+        raise errors.AllCommandsDisabled()
+    name = ctx.command.qualified_name
+    if name in guild["disabled_commands"]:
+        raise errors.CommandDisabled(
+            f"The command {name} has been disabled "
+            "by the moderators of this server."
+        )
+    return True
+
+
+GLOBAL_CHECKS = [
+    not_disabled,
+    commands.bot_has_permissions(send_messages=True),
+]
 
 
 def setup(bot: Bot) -> None:
-    for check in checks.GLOBAL_CHECKS:
+    for check in GLOBAL_CHECKS:
         bot.add_check(check)
