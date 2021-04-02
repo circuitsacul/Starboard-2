0e165be19cf889666bb487da5f355ba78a1e9213
Circuit <circuitsacul@gmail.com>
2021-04-02 11:47:25 -0400

    Add example .po file

---
+++ app/locale/all_files.py
@@ -0,0 +1,9 @@
+import os.path
+
+for dir, subdirs, files in os.walk("."):
+    if "venv" in dir or "locale" in dir:
+        continue
+    for file in files:
+        if file.endswith(".py"):
+            fname = os.path.join(dir, file)
+            print(fname)
