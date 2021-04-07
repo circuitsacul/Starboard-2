d30a3bf50f92ef9c01556d4e0f204f0879550f8d
Circuit <circuitsacul@gmail.com>
2021-04-07 17:14:30 -0400

    unused import

--- app/cogs/settings/settings_commands.py
+++ app/cogs/settings/settings_commands.py
@@ -6,7 +6,7 @@ from discord.ext import commands, flags
 from app import converters, errors, menus, utils
 from app.classes.bot import Bot
 from app.cogs.quick_actions import qa_funcs
-from app.i18n import current_locale, locales, t_
+from app.i18n import locales, t_
 
 
 async def raise_if_exists(emoji: str, ctx: commands.Context) -> None:
