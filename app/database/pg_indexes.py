148dbe166c7adaf47e6d2951a477d373067c1693
Circuit <circuitsacul@gmail.com>
2021-04-09 17:43:11 -0400

    more indexes

--- app/database/pg_indexes.py
+++ app/database/pg_indexes.py
@@ -1,4 +1,15 @@
 MEMBERS__USER_ID__GUILD_ID = """CREATE INDEX IF NOT EXISTS
     members__user_id__guild_id ON members (user_id, guild_id)"""
 
-ALL_INDEXES = [MEMBERS__USER_ID__GUILD_ID]
+STARBOARDS__GUILD_ID = """CREATE INDEX IF NOT EXISTS
+    starboards__guild_id ON starboards USING HASH (guild_id)"""
+
+REACTION_USERS__REACTION_ID__USER_ID = """CREATE UNIQUE INDEX IF NOT EXISTS
+    reaction_users__reaction_id__user_id ON reaction_users
+    (reaction_id, user_id)"""
+
+ALL_INDEXES = [
+    MEMBERS__USER_ID__GUILD_ID,
+    STARBOARDS__GUILD_ID,
+    REACTION_USERS__REACTION_ID__USER_ID,
+]
