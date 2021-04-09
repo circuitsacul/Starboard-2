fc0eec0f68c58588325cab10466df3f0476211dc
Circuit <circuitsacul@gmail.com>
2021-04-09 16:51:22 -0400

    rename index

--- app/database/pg_indexes.py
+++ app/database/pg_indexes.py
@@ -1,4 +1,4 @@
 MEMBERS__USER_ID__GUILD_ID = """CREATE INDEX IF NOT EXISTS
-    members__user_id ON members (user_id, guild_id)"""
+    members__user_id__guild_id ON members (user_id, guild_id)"""
 
 ALL_INDEXES = [MEMBERS__USER_ID__GUILD_ID]
