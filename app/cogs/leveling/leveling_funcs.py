a17b2d2e0cad56802e9b14954ad35e5c1ac54b70
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-04-20 13:16:27 -0400

    make the level formula better

--- app/cogs/leveling/leveling_funcs.py
+++ app/cogs/leveling/leveling_funcs.py
@@ -1,11 +1,4 @@
-from math import sqrt
-
-
-def next_level_xp(current_level: int) -> int:
-    return int((current_level + 1) ** 2)
-
-
 def current_level(xp: int) -> int:
-    if xp < 0:
+    if xp <= 0:
         return 0
-    return int(sqrt(xp))
+    return int((xp / 10) ** 0.3) + 1
