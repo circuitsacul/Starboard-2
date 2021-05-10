947e8f0bac785876f4f150b8db1b10c027eed30a
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-10 18:37:49 -0400

    use double quotes

--- app/dashboard/static/js/base.js
+++ app/dashboard/static/js/base.js
@@ -5,18 +5,18 @@
  * @param {string} [method=post] the method to use on the form
  */
 
-function post(path, params, method='post') {
+function post(path, params, method="post") {
 
     // The rest of this code assumes you are not using a library.
     // It can be made less verbose if you use one.
-    const form = document.createElement('form');
+    const form = document.createElement("form");
     form.method = method;
     form.action = path;
   
     for (const key in params) {
         if (params.hasOwnProperty(key)) {
-            const hiddenField = document.createElement('input');
-            hiddenField.type = 'hidden';
+            const hiddenField = document.createElement("input");
+            hiddenField.type = "hidden";
             hiddenField.name = key;
             hiddenField.value = params[key];
     
@@ -30,10 +30,10 @@ function post(path, params, method='post') {
 
 
 function removeToast(toastId) {
-    $(`#${toastId}`).remove()
+    $("#${toastId}").remove();
 }
 
 
 $(document).ready(function() {
     $(".select2").select2();
-})
+});
