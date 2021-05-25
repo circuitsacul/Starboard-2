bcc268fc0e074193961bf57a3d01ad2474b5f9c0
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-05-25 11:00:59 -0400

    oops

--- app/classes/bot.py
+++ app/classes/bot.py
@@ -54,7 +54,7 @@ class Bot(commands.AutoShardedBot):
                 color=self.theme_color,
                 show_user_perms=True,
                 user_guild_perms_title=t_("Required Permissions", True),
-                usage_string=t_("Usage String", True),
+                usage_string=t_("Usage", True),
                 aliases_string=t_("Aliases", True),
                 command_attrs={
                     "name": "_commands",
