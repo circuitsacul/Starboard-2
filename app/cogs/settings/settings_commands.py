24fe13612274c752a95dea0c49dec2328e4ecf85
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-06-02 20:06:00 -0400

    unused import

--- app/cogs/settings/settings_commands.py
+++ app/cogs/settings/settings_commands.py
@@ -1,16 +1,7 @@
 import discord
 from discord.ext.prettyhelp import bot_has_permissions, has_guild_permissions
 
-from app import (
-    buttons,
-    commands,
-    converters,
-    errors,
-    flags,
-    i18n,
-    menus,
-    utils,
-)
+from app import buttons, commands, converters, errors, flags, i18n, utils
 from app.classes.bot import Bot
 from app.classes.context import MyContext
 from app.cogs.quick_actions import qa_funcs
