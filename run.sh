217ccefe4f2468fa2ea76d481c7043af8cfaa487
CircuitSacul <55721663+CircuitSacul@users.noreply.github.com>
2021-07-01 17:12:54 -0400

    Auto generate MO files in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+$1 app/i18n/generate_mo
 $1 update_db.py
 
 $1 run_ipc.py &
@@ -6,6 +7,8 @@ $1 run_dashboard.py &
 
 while true
 do
+    $1 update_db.py
+    $1 app/i18n/generate_mo
     $1 run_bot.py
 
     echo "Hit CTRL+C to shutdown."
